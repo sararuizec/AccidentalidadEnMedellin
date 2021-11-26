8132cdf297a5864543f8a0b6373654b849a18fc5
Cristian Londoño Franco <clondonof@unal.edu.co>
2021-11-26 01:04:23 -0500

    manejoDB
    scripts para el agrupamiento y la creacion de mapas

--- codigo/Rscript.r
+++ codigo/Rscript.r
@@ -1,6 +1,10 @@
-library(readr)
+require(pacman)
+pacman::p_load(ggplot2, sf, reader)
+rm(list = ls())
+
+
 #dataframe
-acc_med <- read_delim("##",
+acc_med <- read_delim("GitHub/AccidentalidadEnMedellin/incidentes_viales.csv",
                       ";", escape_double = FALSE, col_types = cols(`AÑO` = col_character(),
                                                                    FECHA_ACCIDENTES = col_character()),
                       trim_ws = TRUE)
@@ -20,11 +24,31 @@ acc_med$LOCATION[acc_med$LOCATION %in% c("Corregimiento de San Sebasti\\xE1n de 
 acc_med$LOCATION[acc_med$LOCATION %in% c("La Am\\xE9rica", "La América")] <- "La America"
 
 ##eliminando datos inecesarios
-acc_med <- acc_med[!(acc_med$LOCATION %in% c("SN","Sin Inf","AU","In", "0")), ]
+acc_med <- acc_med[!(acc_med$LOCATION %in% c("SN","Sin Inf","AU","In", "0", NA)), ]
 
 acc_med <- cbind(acc_med, 
                  strcapture('(\\d*[/]\\d*[/]\\d*)', 
                             acc_med$FECHA_ACCIDENTE, data.frame(FECHA = character())))
 
-dbExport <- data.frame(FECHA = acc_med$FECHA, CLASE = acc_med$CLASE_ACCIDENTE, DAÑOS = acc_med$DAÑOS, LOCACION = acc_med$LOCATION)
-write.csv(dbExport, 'db-acc-med.csv')
+#dbExport <- data.frame(FECHA = acc_med$FECHA, CLASE = acc_med$CLASE_ACCIDENTE, DANOS = acc_med$DAÑOS, LOCACION = acc_med$LOCATION)
+#write.csv2(dbExport, 'db-acc-med.csv')
+
+#agrupamiento por barrio
+barrios <- st_read("GitHub/AccidentalidadEnMedellin/shp/Barrio_Vereda_2014.shp")
+barrios <- barrios[ , 3:8]
+colnames(barrios)[colnames(barrios) == "NOMBRE"] <- "BARRIO"
+
+acc_med2 <- acc_med[ , c(1,3,4,6,10,15,16,17,18)]
+colnames(acc_med2)[colnames(acc_med2) == "LOCATION"] <- "BARRIO"
+
+#criterios Clase_acc, daños, frecuencia
+frecuencia <- data.frame(table(acc_med2$BARRIO, acc_med2$CLASE_ACCIDENTE, acc_med2$DAÑOS))
+colnames(frecuencia)<-c("BARRIO","CLASE_ACC","DAÑOS","FRECUENCIA")
+
+#crear mapa
+
+castila <- barrios[barrios$BARRIO == "Castilla", ]
+
+ggplot() +
+  geom_sf(data = barrios,fill="white", color = "black")+
+  geom_sf(data = castila, fill = "red")
